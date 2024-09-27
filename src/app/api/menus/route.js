138b3b96f4bd67b680472fd4579d5f4f74c81b28
missile720 <58495374+missile720@users.noreply.github.com>
2024-09-27 15:59:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/api/menus/route.js
@@ -0,0 +1,84 @@
+import { pool } from "@/data/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { menuName, description, uid } = await req.json();
+
+  if (!menuName || !uid) {
+    return NextResponse.json(
+      { error: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const client = await pool.connect();
+
+    // Fetch the restaurant_id by comparing the user's uid in the Restaurants table
+    const restaurantQuery = await client.query(
+      "SELECT restaurant_id FROM Restaurants WHERE uid = $1",
+      [uid]
+    );
+
+    if (restaurantQuery.rows.length === 0) {
+      client.release();
+      return NextResponse.json(
+        { error: "Restaurant not found for the given user" },
+        { status: 404 }
+      );
+    }
+
+    const restaurantId = restaurantQuery.rows[0].restaurant_id;
+
+    // Insert the new menu into the Menus table
+    const result = await client.query(
+      "INSERT INTO Menus (restaurant_id, name, description) VALUES ($1, $2, $3) RETURNING *",
+      [restaurantId, menuName, description]
+    );
+
+    client.release();
+
+    return NextResponse.json(
+      { message: "Menu created successfully", menu: result.rows[0] },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error creating menu:", error);
+    return NextResponse.json(
+      { error: "Failed to create menu" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const uid = searchParams.get("uid"); // Get uid from query parameters
+
+  if (!uid) {
+    return NextResponse.json(
+      { error: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const client = await pool.connect();
+
+    // Fetch the restaurant_id by comparing the user's uid in the Restaurants table
+    const menus = await client.query(
+      "SELECT * FROM Menus WHERE restaurant_id IN (SELECT restaurant_id FROM Restaurants WHERE uid = $1)",
+      [uid]
+    );
+
+    client.release();
+
+    return NextResponse.json({ menus: menus.rows }, { status: 200 });
+  } catch (error) {
+    console.error("Error creating menu:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch menu" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/photo-types/route.js
@@ -0,0 +1,18 @@
+import { pool } from "@/data/db";
+
+export async function GET(request) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM Photo_Types");
+    client.release();
+    return new Response(JSON.stringify(result.rows), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching photo types:", error);
+    return new Response(
+      JSON.stringify({ message: "Error fetching photo types" }),
+      { status: 500 }
+    );
+  }
+}
