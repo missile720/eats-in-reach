8e8b5d5839b6dee3f16ce78ba9c792981c15d31c
Misael Bolainez <misaelbolainez@gmail.com>
2024-09-28 13:56:56 -0700

    feat(tab): Added logo to tab icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,20 @@
 import './globals.css';
 import { AuthProvider } from '../../context/authContext';
 
+export const metadata = {
+  title: 'Eats in Reach',
+  icons: {
+    icon: '/images/actual_logo.png',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-screen w-full">
+      <head>
+        <link rel="icon" href="/images/actual_logo.png" />
+        <meta name="theme-color" content="#AAD15F" />
+      </head>
       <body className="min-h-screen w-full">
         <AuthProvider>
           {children}
