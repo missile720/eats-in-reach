0318625768c6a68fbaa0b69b6d772217064d582d
Misael Bolainez <misaelbolainez@gmail.com>
2024-09-28 14:09:40 -0700

    fix(metadata): Added the metadata to its own file

---
+++ src/app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+      <>
+        <title>Eats in Reach</title>
+        <meta name="description" content="Find your favorite local vendors and food with Eats in Reach!" />
+        <link rel="icon" href="/images/actual_logo.png" />
+        <meta name="theme-color" content="#AAD15F" />
+      </>
+    );
+  }
+  
